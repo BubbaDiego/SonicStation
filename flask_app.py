0f39d6655a90ea8f283e0ead046d83d7537de26d
bubba <bubba.diego@gmail.com>
2025-01-28 05:19:56 -0800

    position enhancements

--- flask_app.py
+++ flask_app.py
@@ -1055,4 +1055,4 @@ def build_heat_data(positions: List[dict]) -> dict:
 
 # MAIN
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
